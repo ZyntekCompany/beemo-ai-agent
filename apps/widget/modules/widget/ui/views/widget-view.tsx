595b6680ba4665927756997de565c5c3d9367b8a
James Galvis <galvisrodriguez618@gmail.com>
2026-01-12 01:36:06 -0500

    feat 08: widget auth screen

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { WidgetHeader } from "../components/widget-header";
-import { WidgetFooter } from "../components/widget-footer";
+// import { WidgetHeader } from "../components/widget-header";
+// import { WidgetFooter } from "../components/widget-footer";
+import WidgetAuthScreen from "../screens/widget-auth-screen";
 
 interface Props {
   organizationId: string;
@@ -9,14 +10,9 @@ interface Props {
 export function WidgetView({ organizationId }: Props) {
   return (
     <main className="flex h-full min-h-screen flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetHeader>
-        <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
-          <p className="text-3xl">Hi there!</p>
-          <p className="text-lg">How can we help you today?</p>
-        </div>
-      </WidgetHeader>
-      <div className="flex flex-1">Widget View: {organizationId}</div>
-      <WidgetFooter />
+      {/* <div className="flex flex-1">Widget View: {organizationId}</div>
+      <WidgetFooter /> */}
+      <WidgetAuthScreen />
     </main>
   );
 }
